869f3dde1e2e81d8b16f60886f2d4f45e2863925
Jeazi Ricardo <jeazi_ricardo@hotmail.com>
2022-05-10 17:25:03 -0300

    Testa se a página contém um heading h2

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,13 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import About from '../components/About';
+
+describe('Testa o componente <About.js />', () => {
+  test('Testa se a página contém um heading h2 com o texto About Pokédex', () => {
+    renderWithRouter(<About />);
+    const title = screen.getByRole('heading',
+      { level: 2, name: /about pokédex/i });
+    expect(title).toBeInTheDocument();
+  });
+});
