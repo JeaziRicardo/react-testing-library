9b2c7007ce03e808ea13d74f157fed3edd790b21
Jeazi Ricardo <jeazi_ricardo@hotmail.com>
2022-05-10 10:44:57 -0300

    Testa se App tem no topo um conjunto de links

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,16 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import App from '../App';
+
+const navs = [/home/i, /about/i, /favorite pokémons/i];
+
+describe('Testa o componente <App.js />', () => {
+  test('Testa se o topo da aplicação contém um conjunto de links de navegação.', () => {
+    renderWithRouter(<App />);
+    navs.map((nav) => {
+      const link = screen.getByRole('link', { name: nav });
+      return (expect(link).toBeInTheDocument());
+    });
+  });
+});
