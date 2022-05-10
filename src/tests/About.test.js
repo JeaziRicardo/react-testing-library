5e767a82ca2b529f98d3a5fb6b076d71c2289eec
Jeazi Ricardo <jeazi_ricardo@hotmail.com>
2022-05-10 17:44:23 -0300

    Testa se a página contém dois parágrafos com texto sobre a Pokédex

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -10,4 +10,14 @@ describe('Testa o componente <About.js />', () => {
       { level: 2, name: /about pokédex/i });
     expect(title).toBeInTheDocument();
   });
+
+  test('Testa se a página contém dois parágrafos com texto sobre a Pokédex', () => {
+    renderWithRouter(<About />);
+    const textOne = /This application simulates a Pokédex/i;
+    const textTwwo = /One can filter Pokémons by type/i;
+    const paragraphsOne = screen.getByText(textOne);
+    const paragraphsTwo = screen.getByText(textTwwo);
+    expect(paragraphsOne).toBeInTheDocument();
+    expect(paragraphsTwo).toBeInTheDocument();
+  });
 });
