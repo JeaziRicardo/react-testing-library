9b7151d17402524939abeab75efba9b7a885e997
Jeazi Ricardo <jeazi_ricardo@hotmail.com>
2022-05-10 18:49:24 -0300

    Testa se NotFound tem um título específico

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,13 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import NotFound from '../components/NotFound';
+
+describe('Testa o componente <NotFound.js />', () => {
+  test('Testa se a página contém um heading com o texto Page requested not found', () => {
+    renderWithRouter(<NotFound />);
+    const notFoundTitle = screen.getByRole('heading',
+      { level: 2, name: /page requested not found/i });
+    expect(notFoundTitle).toBeInTheDocument();
+  });
+});
