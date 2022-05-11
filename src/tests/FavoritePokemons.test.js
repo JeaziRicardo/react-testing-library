80f23172e926fe603cdb5c051f749ea01f4feeb1
Jeazi Ricardo <jeazi_ricardo@hotmail.com>
2022-05-11 08:31:45 -0300

    Testa se aparece uma msg em FavoritePokemons quando ñ tem favoritos

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,12 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import FavoritePokemons from '../components/FavoritePokemons';
+
+describe('Testa o componente <FavoritePokemons.js />', () => {
+  test('Testa se a mensagem No favorite pokemon found, caso não tenha pokémons', () => {
+    renderWithRouter(<FavoritePokemons />);
+    const mensage = screen.getByText('No favorite pokemon found');
+    expect(mensage).toBeInTheDocument();
+  });
+});
