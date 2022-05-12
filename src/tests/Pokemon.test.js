e4f29b99aa4b7780e982af31412faf8c51b815d9
Jeazi Ricardo <jeazi_ricardo@hotmail.com>
2022-05-12 00:24:56 -0300

    Testa se o componente Pokemon.js renderiza o link More Details e o seu funcionamento

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import renderWithRouter from '../renderWithRouter';
 import App from '../App';
 
@@ -18,5 +19,19 @@ describe('Teste o componente <Pokemon.js />', () => {
     const pokémonImage = screen.getByRole('img');
     expect(pokémonImage).toHaveAttribute('src', 'https://cdn2.bulbagarden.net/upload/b/b2/Spr_5b_025_m.png');
     expect(pokémonImage).toHaveAttribute('alt', 'Pikachu sprite');
+  });
+
+  test('Testa o link More Details no card do pokémon', () => {
+    const { history } = renderWithRouter(<App />);
+    const linkDetails = screen.getByRole('link', { name: 'More details' });
+    expect(linkDetails).toBeInTheDocument();
+    userEvent.click(linkDetails);
+
+    const titleDetails = screen.getByRole('heading',
+      { level: 2, name: 'Pikachu Details' });
+    expect(titleDetails).toBeInTheDocument();
+
+    const { pathname } = history.location;
+    expect(pathname).toBe('/pokemons/25');
   });
 });
