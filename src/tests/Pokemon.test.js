1bd05fafcb09585cbb64add931628350c8c9a560
Jeazi Ricardo <jeazi_ricardo@hotmail.com>
2022-05-12 00:00:12 -0300

    Testa se o componente Pokemon.js renderiza o card com as informações corretas do pokémon

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -1,1 +1,22 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import App from '../App';
+
+describe('Teste o componente <Pokemon.js />', () => {
+  test('Testa se é renderizado um card com as informações do pokémon.', () => {
+    renderWithRouter(<App />);
+    const pokémonName = screen.getByText('Pikachu');
+    expect(pokémonName).toBeInTheDocument();
+
+    const pokémonType = screen.getAllByText('Electric');
+    expect(pokémonType[0]).toBeInTheDocument();
+
+    const averageWeight = screen.getByText('Average weight: 6.0 kg');
+    expect(averageWeight).toBeInTheDocument();
+
+    const pokémonImage = screen.getByRole('img');
+    expect(pokémonImage).toHaveAttribute('src', 'https://cdn2.bulbagarden.net/upload/b/b2/Spr_5b_025_m.png');
+    expect(pokémonImage).toHaveAttribute('alt', 'Pikachu sprite');
+  });
+});
