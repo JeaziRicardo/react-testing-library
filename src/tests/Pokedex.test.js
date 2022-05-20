e2584aab6a6fca6bc5a113b78357352f5e5bbd50
Jeazi Ricardo <jeazi_ricardo@hotmail.com>
2022-05-20 17:40:07 -0300

    Testa se o próximo pokemon é exibido ao clicar no botão de próximo

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,12 +1,26 @@
 import React from 'react';
 import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import renderWithRouter from '../renderWithRouter';
 import App from '../App';
+import pokemons from '../data';
 
 describe('Testa o componente <Pokedex.js />', () => {
   test('Testa se a página contém um heading h2 com o texto Encountered pokémons', () => {
     renderWithRouter(<App />);
     const title = screen.getByRole('heading', { level: 2, name: 'Encountered pokémons' });
     expect(title).toBeInTheDocument();
+  });
+
+  test('Testa se é exibido o próximo pokémon da lista quando o botão é clicado', () => {
+    renderWithRouter(<App />);
+    pokemons.forEach(({ name }) => {
+      const namePokemon = screen.getByText(name);
+      const btnNext = screen.getByText(/próximo/i);
+      expect(namePokemon).toBeInTheDocument();
+      userEvent.click(btnNext);
+    });
+    const namePokemon = screen.getByText('Pikachu');
+    expect(namePokemon).toBeInTheDocument();
   });
 });
